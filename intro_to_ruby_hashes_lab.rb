9a9ea80b9f03a3557feaad146e45c7b26c77bb25
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 21:13:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,8 @@ def monopoly_with_second_tier
 	monopoly = {}
 	monopoly.store(railroads: {}) 
 	monopoly['railroads'][pieces] = 4
-	monopoly['railroads'][[names] = {}
-	monopoly['railroads'][rent_in_dollars] = {}
+	monopoly['railroads'][[names] = hash.new 
+	monopoly['railroads'][rent_in_dollars] = hash.new
 end
 
 def monopoly_with_third_tier
