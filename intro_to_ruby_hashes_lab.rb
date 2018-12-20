40ebacba950dde5fc08a41c22c20353c45c3c7c6
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 20:59:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,8 @@ def monopoly_with_second_tier
 	monopoly = {}
 	monopoly[:railroads] = {}
 	monopoly[:railroads][:pieces] = 4
+	monopoly[:railroads][:names] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
 end
 
 def monopoly_with_third_tier
